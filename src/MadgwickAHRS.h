2d0155a4a8fae9ad7fc6ce9202873bfb577ebaaa
zschira <zasc3143@colorado.edu>
2018-07-05 15:55:22 -0700

    fixed function declaration

--- src/MadgwickAHRS.h
+++ src/MadgwickAHRS.h
@@ -22,8 +22,7 @@ extern volatile float beta;				// algorithm gain
 //---------------------------------------------------------------------------------------------------
 // Function declarations
 
-void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
-void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
+void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz, float quaternion[4]);
 
 #ifdef __cplusplus
 }
