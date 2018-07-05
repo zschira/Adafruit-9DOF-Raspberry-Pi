97a5a9bbc23fdfb7364372e871295a5301a16450
zschira <zasc3143@colorado.edu>
2018-07-05 15:58:16 -0700

    removed imu function without mag

--- src/MadgwickAHRS.c
+++ src/MadgwickAHRS.c
@@ -48,11 +48,11 @@ void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float 
 	float hx, hy;
 	float _2q0mx, _2q0my, _2q0mz, _2q1mx, _2bx, _2bz, _4bx, _4bz, _2q0, _2q1, _2q2, _2q3, _2q0q2, _2q2q3, q0q0, q0q1, q0q2, q0q3, q1q1, q1q2, q1q3, q2q2, q2q3, q3q3;
 
-	// Use IMU algorithm if magnetometer measurement invalid (avoids NaN in magnetometer normalisation)
+	/*// Use IMU algorithm if magnetometer measurement invalid (avoids NaN in magnetometer normalisation)
 	if((mx == 0.0f) && (my == 0.0f) && (mz == 0.0f)) {
 		MadgwickAHRSupdateIMU(gx, gy, gz, ax, ay, az);
 		return;
-	}
+	}*/
 
 	// Rate of change of quaternion from gyroscope
 	qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
