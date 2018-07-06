61aa1dc00e30ddeaa341754497097380c371b075
zschira <zasc3143@colorado.edu>
2018-07-06 16:54:36 -0700

    print quaternion

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main() {
     imu.setAutoRange();
     int counter = 0;
     float quaternion[4] = {1.0, 0.0, 0.0, 0.0};
-    while(counter < 100) {
+    while(counter < 10000) {
         imu.calcCoord(quaternion);
+        printf("%f   %f   %f   %f\n", quaternion[0], quaternion[1], quaternion[2], quaternion[3]);
         //Sleep for 0.25ms
 		//usleep(250000);
         counter++;
