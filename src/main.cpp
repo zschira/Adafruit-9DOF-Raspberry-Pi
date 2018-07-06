7a1cfe35a77447e2e1db60db79b97dd286aa2c7e
zschira <zasc3143@colorado.edu>
2018-07-06 12:13:03 -0700

    Revert "Testing realtime orientation rendering"
    This reverts commit 63b3e56ecadff7e3811e20de2e29da9fbec8ad78.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
-#include "render.h"
+#include "ADAFRUIT_9DOF.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-    Render scene;
-    scene.run();
+    ADAFRUIT_9DOF imu;
+    imu.setAutoRange();
+    int counter = 0;
+    float heading;
+    while(counter < 100) {
+        imu.readAll();
+        imu.calcCoord();
+        cout << "Roll: " << imu.roll << endl;
+        cout << "Pitch: " << imu.pitch << endl;
+        cout << "Yaw: " << imu.yaw << endl;
+        //Sleep for 0.25ms
+		//usleep(250000);
+        counter++;
+    }
 }
